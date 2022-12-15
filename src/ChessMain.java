524cd7f5c938cbea0e9d0dab20594ee42db6693a
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-12-15 16:15:23 -0500

    fff

--- src/ChessMain.java
+++ src/ChessMain.java
@@ -11,13 +11,7 @@ import javax.swing.*;
  * @version 2010.11.17
  */
 public class ChessMain{
-    // ----------------------------------------------------------
-    /**
-     * Creates the GUI for Chess.
-     * 
-     * @param args
-     *            command line arguments, not used
-     */
+
     public static void main( String[] args ){
         JFrame frame = new JFrame( "YetAnotherChessGame 1.0" );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
